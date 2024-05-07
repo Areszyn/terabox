a6175a414f6f4036ff219acbd99fa769ef8dc818
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2024-05-07 19:37:32 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -285,10 +285,10 @@ async def get_data(link_data):
            download_link = url + link_data["dlink"][link_data["dlink"].index("/", 8):]
    # download_link = await shorten_url(download_link)
     thumb = link_data["thumbs"]["url3"]
-    return file_name, file_size, link_data["size"], download_link, thumb
+    return file_name, file_size, link_data["size"], download_link, thumb, link_data["dlink"]
   except Exception as e:
     print(e)
-    return None, None, None, None, None
+    return None, None, None, None, None, None
 
 async def extract_links(message):
     # fetch all links
