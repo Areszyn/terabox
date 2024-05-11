7c4e511c15fb78b77f2d90873c22f74112df4206
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2024-05-11 13:56:42 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -263,8 +263,8 @@ async def get_direct_link(url):
         async with my_session.head(url) as response:
             response.raise_for_status()
             direct_link = response.headers.get('Location')
-       #     download_link = "https://d3.terabox.app" + direct_link[direct_link.index("/", 8):]
-            return direct_link            
+            download_link = "https://d3.terabox.app" + direct_link[direct_link.index("/", 8):]
+            return download_link            
     except Exception as e:
         print(f"Error fetching direct link: {e}")
         return None
