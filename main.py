0550941655b29c686b6d8adfaa05e55834bf77db
PuneetStar <99722168+PuneetStar@users.noreply.github.com>
2024-05-13 18:45:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -308,7 +308,7 @@ async def terabox_dm(client, message):
                    await message.reply_text("Some Error Occurred", quote=True)
                    continue 
                 for link in link_data:
-                    name, size, size_bytes, dlink, dlink2, thumb  = await get_data(link)
+                    name, size, size_bytes, dlink, dlink2, dlink3, thumb  = await get_data(link)
                     if dlink:
                       try:                        
                          ril = await client.send_video(-1002117106922, dlink, caption="Indian")
@@ -323,7 +323,7 @@ async def terabox_dm(client, message):
                            await asyncio.sleep(e.value)
                       except Exception as e:
                          print(e)
-                         await client.send_photo(message.chat.id, thumb, has_spoiler=True, caption=f"**Title**: `{name}`\n**Size**: `{size}`\n**Download Link V1**: [Link]({dlink})\n**Download Link V2**: [Link]({dlink2})\n**How To Watch Video**: [Here](https://t.me/TeraBoxHelper/2)")
+                         await client.send_photo(message.chat.id, thumb, has_spoiler=True, caption=f"**Title**: `{name}`\n**Size**: `{size}`\n**Download Link V1**: [Link]({dlink})\n**Download Link V2**: [Link]({dlink2})\n**Download Link V3**: [Link]({dlink3})\n**How To Watch Video**: [Here](https://t.me/TeraBoxHelper/2)")
                          await nil.edit_text("Completed")
         except FloodWait as e:
             await asyncio.sleep(e.value)
